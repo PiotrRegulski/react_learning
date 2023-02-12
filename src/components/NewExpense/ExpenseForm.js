d01f8c01f9d33bafe38409d5b776db86aad24c5b
Piotr Regulski <bulu901@gmail.com>
2023-02-12 17:22:54 +0100

    add form component

---
+++ src/components/NewExpense/ExpenseForm.js
@@ -0,0 +1,11 @@
+import React from "react";
+import ExpenseDate from "../Expenses/ExpenseDate";
+import "./ExpenseForm.css"
+const ExpenseForm=()=>{
+return(
+    <form>
+
+    </form>
+)
+}
+export default ExpenseForm;
--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -1,12 +1,12 @@
 import React from "react";
-
+import "./NewExpense.css";
 
 export default function NewExpenses(){
     return(
         
-        <form>
-           <input></input> 
-        </form>
+   <div className="new-expense">
+    
+   </div>
        
     )
 }
